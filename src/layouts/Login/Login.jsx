2e301bb23e7568b5315c5640dee342c7b28c0126
kuangguoliang <kuangguoliang@inman.cc>
2018-09-30 17:50:24 +0800

    add form table

--- src/layouts/Login/Login.jsx
+++ src/layouts/Login/Login.jsx
@@ -13,6 +13,9 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 import axios from "axios";
+import { Alert } from 'antd';
+import 'antd/dist/antd.css'
+
 
 
 
@@ -23,90 +26,120 @@ const styles = theme => ({
         marginLeft: theme.spacing.unit * 3,
         marginRight: theme.spacing.unit * 3,
         [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-          width: 400,
-          marginLeft: 'auto',
-          marginRight: 'auto',
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
         },
-      },
-      paper: {
+    },
+    paper: {
         marginTop: theme.spacing.unit * 8,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-      },
-      avatar: {
+    },
+    avatar: {
         margin: theme.spacing.unit,
         backgroundColor: theme.palette.secondary.main,
-      },
-      form: {
+    },
+    form: {
         width: '100%', // Fix IE11 issue.
         marginTop: theme.spacing.unit,
-      },
-      submit: {
+    },
+    submit: {
         marginTop: theme.spacing.unit * 3,
-      },
+    },
+    warning: {
+        margin: theme.spacing.unit
+    }
 });
 
-class SignIn extends React.Component{
+class SignIn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            signIn:"登录",
-            address: "账号",
-            password: "密码",
+            warndisplay: "none",
         };
+
     }
-    handleSubmit = (e) =>{
+    handleSubmit = (event) => {
         axios.get('http://localhost:8001').then(function (response) {
             console.log(response);
         })
-        this.props.history.push('/system/dashboard')
+        if (this.state.email == 'admin' && this.state.password == '123456') {
+            this.props.history.push('/system/dashboard')
+        }
+        else {
+            this.setState({ warndisplay: "block" })
+        }
+        event.preventDefault();
+
+
     }
 
-    render () {
+    handleChange = (event) => {
+        let a = event.target.id
+        this.setState({ [event.target.id]: event.target.value })
+    }
+    render() {
 
         const { classes } = this.props
-        return(
+        return (
             <React.Fragment>
                 <CssBaseline />
                 <main className={classes.layout}>
                     <Paper className={classes.paper}>
                         <Avatar className={classes.avatar}>
-                            <LockIcon  />
+                            <LockIcon />
                         </Avatar>
-                        <Typography variant="headline">{this.state.signIn}</Typography>
-                            <form className={classes.form} 
+                        <Typography variant="headline">登录</Typography>
+                        <div className={classes.warning} >
+                            <Alert
+                                message="登录错误"
+                                description="账号和密码不匹配，请重新尝试！"
+                                type="error"
+                                showIcon
+                                style={{display: this.state.warndisplay}}
+                            />
+                        </div>
+                        <form className={classes.form}
                             onSubmit={this.handleSubmit}
-                            >
-                                <FormControl margin="normal" required fullWidth>
-                                <InputLabel htmlFor="email">{this.state.address}</InputLabel>
-                                <Input id="email" name="email" autoComplete="email" autoFocus />
-                                </FormControl>
-                                <FormControl margin="normal" required fullWidth>
-                                <InputLabel htmlFor="password">{this.state.password}</InputLabel>
+                        >
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">账号</InputLabel>
+                                <Input id="email"
+                                    name="email"
+                                    autoComplete="email"
+                                    // value={this.state.email}
+                                    onChange={this.handleChange}
+                                    autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">密码</InputLabel>
                                 <Input
                                     name="password"
                                     type="password"
                                     id="password"
+                                    // value={this.state.email}
+                                    onChange={this.handleChange}
                                     autoComplete="current-password"
                                 />
-                                </FormControl>
-                                <Button
+                            </FormControl>
+                            <Button
                                 type="submit"
                                 // type="button"
                                 fullWidth
                                 variant="raised"
                                 color="primary"
                                 className={classes.submit}
-                                // onClick={this.handleSubmit}
-                                >
-                                {this.state.signIn}
-                                </Button>
-                            </form>
+                            // onClick={this.handleSubmit}
+                            >
+                                登录
+                            </Button>
+                        </form>
                     </Paper>
                 </main>
-                
+
             </React.Fragment>
         );
     }
@@ -114,7 +147,7 @@ class SignIn extends React.Component{
 
 SignIn.propTypes = {
     classes: PropTypes.object.isRequired
-  };
-  
+};
+
 
 export default withStyles(styles)(SignIn);
